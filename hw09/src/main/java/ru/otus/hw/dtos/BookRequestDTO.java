2a8bc66553906c5042810c3a6cc829c28c5c899b
Panfilov Artur <mrpanfilov@gmail.com>
2024-04-22 22:27:06 +0300

    hw09 test

--- hw09/src/main/java/ru/otus/hw/dtos/BookRequestDTO.java
+++ hw09/src/main/java/ru/otus/hw/dtos/BookRequestDTO.java
@@ -3,6 +3,7 @@ package ru.otus.hw.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.Set;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class BookRequestDTO {
 
     private Long id;
--- hw09/src/main/java/ru/otus/hw/dtos/GenreDTO.java
+++ hw09/src/main/java/ru/otus/hw/dtos/GenreDTO.java
@@ -1,12 +1,13 @@
 package ru.otus.hw.dtos;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class GenreDTO {
 
     private Long id;
