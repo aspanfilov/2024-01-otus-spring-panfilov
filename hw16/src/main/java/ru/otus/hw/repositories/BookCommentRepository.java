969b05f5554ed9534c8794ad7e45f48d9362f9dc
Panfilov Artur <mrpanfilov@gmail.com>
2024-06-09 01:59:01 +0300

    hw16 data rest

--- hw16/src/main/java/ru/otus/hw/repositories/BookCommentRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/BookCommentRepository.java
@@ -2,11 +2,16 @@ package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.BookComment;
 
 import java.util.List;
 import java.util.Optional;
 
+@RepositoryRestResource(
+        path = "book-comments",
+        collectionResourceRel = "book-comments",
+        itemResourceRel = "book-comment")
 public interface BookCommentRepository extends JpaRepository<BookComment, Long> {
 
     @EntityGraph(attributePaths = {"book"})
