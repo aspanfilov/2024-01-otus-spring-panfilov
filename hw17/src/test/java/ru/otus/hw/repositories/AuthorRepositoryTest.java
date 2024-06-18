172fa66ec9d5827b112c533f56233e581801087a
Panfilov Artur <mrpanfilov@gmail.com>
2024-06-19 00:02:04 +0300

    hw17 testcontainers

--- hw17/src/test/java/ru/otus/hw/repositories/AuthorRepositoryTest.java
+++ hw17/src/test/java/ru/otus/hw/repositories/AuthorRepositoryTest.java
@@ -1,13 +1,11 @@
 package ru.otus.hw.repositories;
 
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 import ru.otus.hw.BaseContainerTest;
 import ru.otus.hw.models.Author;
 
